cb1b9dbf06fb595f6263c790ffe9f6f152c57c89
Drate Berry <drate@convergecore.com>
2026-03-26 09:51:09 -0500

    Add pm2 support

---
+++ ecosystem.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "video-host",
+      script: "node_modules/.bin/next",
+      args: "start -p 3001",
+      cwd: __dirname,
+      env: {
+        NODE_ENV: "production",
+      },
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "512M",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+    },
+  ],
+};
